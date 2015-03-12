1bd8daca86246a49cedab39ad5f5b5b91de92148
Justin Haar <justin.haar22@gmail.com>
2015-03-12 14:54:44 -0700

    First Commit

--- TicTacToe/ViewController.m
+++ TicTacToe/ViewController.m
@@ -10,6 +10,17 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UILabel *labelOne;
+@property (weak, nonatomic) IBOutlet UILabel *labelTwo;
+@property (weak, nonatomic) IBOutlet UILabel *labelThree;
+@property (weak, nonatomic) IBOutlet UILabel *labelFour;
+@property (weak, nonatomic) IBOutlet UILabel *labelFive;
+@property (weak, nonatomic) IBOutlet UILabel *labelSix;
+@property (weak, nonatomic) IBOutlet UILabel *labelSeven;
+@property (weak, nonatomic) IBOutlet UILabel *labelEight;
+@property (weak, nonatomic) IBOutlet UILabel *labelNine;
+@property (weak, nonatomic) IBOutlet UILabel *whichPlayerLabel;
+
 @end
 
 @implementation ViewController
@@ -23,5 +34,17 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (void)findLabelUsingPoint:(CGPoint)point
+{
+    
+}
+
+- (IBAction)onLabelTapped:(UITapGestureRecognizer *)sender
+{
+    CGPoint point = [sender locationInView:self.view];
+
+}
+
 
 @end
